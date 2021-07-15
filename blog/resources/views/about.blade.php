fc935764224a2eddbfeeb521c9210e1d6ffdbcfb
andrew21-mch <nfonandrew73@gmail.com>
2021-07-15 23:19:09 +0100

    centralised stuffs

--- blog/resources/views/about.blade.php
+++ blog/resources/views/about.blade.php
@@ -8,7 +8,7 @@ body{
     </style>
 </head>
 <body>
-    <h1>About Page</h1> <hr>
+   <center><h1>About Page</h1> <hr>
     <h4>This is me learning laravel after using the whole day to install php extensions and dependencis</h4>
-</body>
+</center> </body>
 </html>
