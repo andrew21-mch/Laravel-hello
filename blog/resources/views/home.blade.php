5bddcac970275633a4ac0543f372eeb41384987d
andrew21-mch <nfonandrew73@gmail.com>
2021-07-15 23:15:52 +0100

    installed Laravel

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<html>
+    
+    <head>
+    <title>Home | Laravel</title>
+
+    <style>
+    body{
+        background-color: blue
+    }
+    </style>
+    </head>
+    <body>
+        <h1>Home page</h1> <hr>
+        <h4>
+        Welcome Laravel
+        </h4>
+    </body>
+</html>
