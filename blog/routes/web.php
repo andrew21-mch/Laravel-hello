e7f1b841f8035b7ae4a73b6f1fc41e04ad071e96
andrew21-mch <nfonandrew73@gmail.com>
2021-07-16 23:21:05 +0100

    update routes

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -13,12 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', function(){
+Route::get('/', function(){
     return view('home');
 });
-Route::get('/about', function(){
-    return view('about');
+Route::get('/contact', function(){
+    return view('contact');
 });
