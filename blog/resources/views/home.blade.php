890a03c9b358467e55d61006064ee089896f6edc
andrew21-mch <nfonandrew73@gmail.com>
2021-07-16 23:20:45 +0100

    updated home page

--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -5,14 +5,71 @@
 
     <style>
     body{
-        background-color: smoke;
+        background-color: indigo;
+        width: 98%
+    }
+    img{
+        width: 20rem;
+        text-align: center;
+    }
+    .nav-img{
+        width: 5rem;
+        height: 5rem;
+        border-radius: 50px 50px;
+        float: left
+    }
+    a{
+        float: right;
+        padding-top: 3%;
+        padding-left: 2%;
+        padding-right: 2%;
+    }
+    .about{
+        width: 85%;
+        background-color: white;
+        
+    }
+
+    nav{
+        padding-bottom: 5rem;
+        background-color:violet;
+    }
+    p{
+        text-align: left;
+        width: 85%;
+    }
+    button{
+        width: 10rem;
+        height: 3rem;
+        background-color: red;
+        border: none;
+        border-radius: 4px;
     }
     </style>
     </head>
     <body>
-        <center><h1>Home page</h1> <hr>
-        <h4>
-        Welcome Laravel
-        </h4></center>
+    <nav>
+        <img class="nav-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png" alt="laravel">
+       <a href="#">LARAVEL HOME</a><a href="#about">About</a><a href="home">Home</a> <a href="contact">contact</a></nav>
+        <center><h1>Home page</h1> 
+        <hr style="width: 30rem">
+       <div class="intro"><h4>
+        Hi there I just started Laravel, hoping its going to be fun.</h4>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa alias illum aspernatur, quia soluta eos quos id rem dolor dignissimos quisquam obcaecati aliquam iste doloribus quaerat, debitis sapiente laudantium quis!
+        </p> 
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sit veritatis nisi, nostrum mollitia, ratione nam accusantium vel omnis iure ad molestiae, similique pariatur exercitationem non beatae praesentium. Dicta, aliquid.</p></center>
+    </div>
+    <center><div class="about" ><h1 id="about">About Laravel</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png" alt="laravel">
+    <p>Laravel is a web application framework with expressive, elegant syntax. ... Laravel attempts to take the pain out of development by easing common tasks used in the majority of web projects, such as authentication, routing, sessions, and caching.</p>
+    
+    </div> 
+    
+    <button>Join Us</button>
+    
+    </center>
+
+    
+    
     </body>
 </html>
