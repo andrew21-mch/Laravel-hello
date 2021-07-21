708ddc290a0da2bc49df948c093de68b8b8c1bf4
andrew21-mch <nfonandrew73@gmail.com>
2021-07-21 17:06:27 +0100

    echoed the session on the user dashboard

--- tasks/learning/resources/views/user.blade.php
+++ tasks/learning/resources/views/user.blade.php
@@ -5,7 +5,7 @@
     <title>Dashboard</title>
   </head>
   <body>
-    <h2>Hello {{session('username')}} </h2>
+    <h2> Hello {{session('username')}} </h2>
     <h4>welcome to your dashboard</h4>
   </body>
 </html>
